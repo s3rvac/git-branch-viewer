1af505acf0f1d40c9f657eb4cb5918f01d256f7a
Petr Zemek <s3rvac@gmail.com>
2014-05-08 13:20:27 +0200

    run-web-server.py: Add a description.

--- run-web-server.py
+++ run-web-server.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+#
+# Starts a local web server on 127.0.0.1:5000 with auto-reload that can be used
+# during the development.
+#
+# Warning: Do NOT use it in production because it may allow attackers to
+# execute arbitrary code on the server!
+#
+# Copyright: (c) 2014 by Petr Zemek <s3rvac@gmail.com> and contributors
+# License: BSD, see LICENSE for more details
+#
 
 from viewer.web import app
 
