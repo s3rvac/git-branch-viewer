1cbaea316dddb61e3fc5fff7e941b4874d70bb87
Petr Zemek <s3rvac@gmail.com>
2014-05-09 13:48:37 +0200

    run-web-server.py: Rephrase the note in the description.

--- run-web-server.py
+++ run-web-server.py
@@ -3,8 +3,8 @@
 # Starts a local web server on 127.0.0.1:5000 with auto-reload that can be used
 # during the development.
 #
-# Warning: Do NOT use it in production because it may allow attackers to
-# execute arbitrary code on the server!
+# Warning: Do NOT use this local server in production because it may allow
+# attackers to execute arbitrary code on the server!
 #
 # Copyright: (c) 2014 by Petr Zemek <s3rvac@gmail.com> and contributors
 # License: BSD, see LICENSE for more details
