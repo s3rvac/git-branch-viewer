b905ff972197a2b743fce79cd399ec54ef55a11d
Petr Zemek <s3rvac@gmail.com>
2014-05-11 18:14:10 +0200

    test_git: Move the patches before the testing class.

--- tests/test_git.py
+++ tests/test_git.py
@@ -14,9 +14,9 @@ from viewer.git import Git
 from viewer.git import NoGitRepositoryError
 
 
+@mock.patch('os.chdir')
+@mock.patch('subprocess.check_call')
 class GitCreateTests(unittest.TestCase):
-    @mock.patch('os.chdir')
-    @mock.patch('subprocess.check_call')
     def test_create_git_from_existing_repository_enters_the_repo_and_calls_git_status(
             self, mock_check_call, mock_chdir):
         REPO_PATH = '/path/to/existing/repository'
@@ -24,19 +24,17 @@ class GitCreateTests(unittest.TestCase):
         mock_chdir.assert_any_call(REPO_PATH)
         mock_check_call.assert_called_once_with(['git', 'status'])
 
-    @mock.patch('os.chdir',
-        side_effect=FileNotFoundError('No such file or directory'))
-    def test_create_git_from_nonexisting_location_raises_exception(self,
-            mock_chdir):
+    def test_create_git_from_nonexisting_location_raises_exception(
+            self, mock_check_call, mock_chdir):
+        mock_chdir.side_effect = FileNotFoundError('No such file or directory')
         REPO_PATH = '/path/to/nonexisting/location'
         self.assertRaises(FileNotFoundError, Git, REPO_PATH)
 
-    @mock.patch('os.chdir')
-    @mock.patch('subprocess.check_call',
-        side_effect=subprocess.CalledProcessError(128, "['git', 'status']",
-            b'fatal: Not a git repository (or any parent up to mount point)'))
-    def test_create_git_from_location_with_no_repository_raises_exception(self,
-            mock_check_call, mock_chdir):
+    def test_create_git_from_location_with_no_repository_raises_exception(
+            self, mock_check_call, mock_chdir):
+        mock_check_call.side_effect = subprocess.CalledProcessError(
+            128, "['git', 'status']",
+            b'fatal: Not a git repository (or any parent up to mount point)')
         REPO_PATH = '/path/to/existing/location/with/norepository'
         self.assertRaises(NoGitRepositoryError, Git, REPO_PATH)
         mock_check_call.assert_called_once_with(['git', 'status'])
