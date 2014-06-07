8471be7ba0460230a328a3c67a50a66b517db066
Petr Zemek <s3rvac@gmail.com>
2014-06-07 09:27:45 +0200

    format: Add a new function: format_age().

--- tests/format_tests.py
+++ tests/format_tests.py
@@ -8,7 +8,84 @@
 import unittest
 import datetime
 
+from viewer.format import format_age
 from viewer.format import format_date
+
+
+class FormatAgeTests(unittest.TestCase):
+    """Tests for format_age()."""
+
+    def test_returns_correctly_formatted_age_zero_seconds(self):
+        age = datetime.timedelta(seconds=0)
+        self.assertEqual(format_age(age), '0 seconds')
+
+    def test_returns_correctly_formatted_age_one_second(self):
+        age = datetime.timedelta(seconds=1)
+        self.assertEqual(format_age(age), '1 second')
+
+    def test_returns_correctly_formatted_age_two_seconds(self):
+        age = datetime.timedelta(seconds=2)
+        self.assertEqual(format_age(age), '2 seconds')
+
+    def test_returns_correctly_formatted_age_max_seconds(self):
+        age = datetime.timedelta(seconds=59)
+        self.assertEqual(format_age(age), '59 seconds')
+
+    def test_returns_correctly_formatted_age_one_minute(self):
+        age = datetime.timedelta(minutes=1)
+        self.assertEqual(format_age(age), '1 minute')
+
+    def test_returns_correctly_formatted_age_one_minute_and_some_seconds(self):
+        age = datetime.timedelta(minutes=1, seconds=2)
+        self.assertEqual(format_age(age), '1 minute')
+
+    def test_returns_correctly_formatted_age_two_minutes(self):
+        age = datetime.timedelta(minutes=2)
+        self.assertEqual(format_age(age), '2 minutes')
+
+    def test_returns_correctly_formatted_age_two_minutes_and_some_seconds(self):
+        age = datetime.timedelta(minutes=2, seconds=5)
+        self.assertEqual(format_age(age), '2 minutes')
+
+    def test_returns_correctly_formatted_age_max_minutes(self):
+        age = datetime.timedelta(minutes=59, seconds=59)
+        self.assertEqual(format_age(age), '59 minutes')
+
+    def test_returns_correctly_formatted_age_one_hour(self):
+        age = datetime.timedelta(hours=1)
+        self.assertEqual(format_age(age), '1 hour')
+
+    def test_returns_correctly_formatted_age_one_hour_and_some_minutes(self):
+        age = datetime.timedelta(hours=1, minutes=5)
+        self.assertEqual(format_age(age), '1 hour')
+
+    def test_returns_correctly_formatted_age_two_hours(self):
+        age = datetime.timedelta(hours=2)
+        self.assertEqual(format_age(age), '2 hours')
+
+    def test_returns_correctly_formatted_age_two_hours_and_some_minutes(self):
+        age = datetime.timedelta(hours=2, minutes=5)
+        self.assertEqual(format_age(age), '2 hours')
+
+    def test_returns_correctly_formatted_age_max_hours(self):
+        age = datetime.timedelta(hours=23, minutes=59, seconds=59)
+        self.assertEqual(format_age(age), '23 hours')
+
+    def test_returns_correctly_formatted_age_one_day(self):
+        age = datetime.timedelta(days=1)
+        self.assertEqual(format_age(age), '1 day')
+
+    def test_returns_correctly_formatted_age_one_day_and_some_hours(self):
+        age = datetime.timedelta(days=1, hours=5)
+        self.assertEqual(format_age(age), '1 day')
+
+    def test_returns_correctly_formatted_age_two_days(self):
+        age = datetime.timedelta(days=2)
+        self.assertEqual(format_age(age), '2 days')
+
+    def test_returns_correctly_formatted_age_two_days_and_some_hours(self):
+        age = datetime.timedelta(days=2, hours=5)
+        self.assertEqual(format_age(age), '2 days')
 
 
 class FormatDateTests(unittest.TestCase):
--- viewer/format.py
+++ viewer/format.py
@@ -9,6 +9,25 @@
 """
 
 
+def format_age(age):
+    """Formats the given age (:class:`timedelta` object).
+
+    :returns: `age` of the form `'X seconds/hours/days'`.
+    """
+    # The timedelta internal representation uses only days and seconds (and
+    # microseconds, but we do not care about them).
+    if age.days > 0:
+        repr = '{} days'.format(age.days)
+    elif age.seconds >= 3600:
+        repr = '{} hours'.format(age.seconds // 3600)
+    elif age.seconds >= 60:
+        repr = '{} minutes'.format(age.seconds // 60)
+    else:
+        repr = '{} seconds'.format(age.seconds)
+    # Remove the ending 's' if we have only 1 second/minute/hour/day.
+    return repr if not repr.startswith('1 ') else repr[:-1]
+
+
 def format_date(date):
     """Formats the given date (:class:`datetime` object).
 
