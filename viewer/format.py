fadc1ac91fed0c9c32997562d3e204fb6d3e77e5
Petr Zemek <s3rvac@gmail.com>
2014-06-07 10:18:43 +0200

    format: Add support for formatting negative ages.

--- viewer/format.py
+++ viewer/format.py
@@ -13,19 +13,30 @@ def format_age(age):
     """Formats the given age (:class:`timedelta` object).
 
     :returns: `age` of the form `'X seconds/hours/days'`.
+
+    When `age` is negative, the minus sign (`-`) is prepended before the
+    returned string.
     """
-    # The timedelta internal representation uses only days and seconds (and
-    # microseconds, but we do not care about them).
-    if age.days > 0:
-        repr = '{} days'.format(age.days)
-    elif age.seconds >= 3600:
-        repr = '{} hours'.format(age.seconds // 3600)
-    elif age.seconds >= 60:
-        repr = '{} minutes'.format(age.seconds // 60)
-    else:
-        repr = '{} seconds'.format(age.seconds)
-    # Remove the ending 's' if we have only 1 second/minute/hour/day.
-    return repr if not repr.startswith('1 ') else repr[:-1]
+    def format_nonnegative_age(age):
+        # The timedelta internal representation uses only days and seconds (and
+        # microseconds, but we do not care about them).
+        if age.days > 0:
+            repr = '{} days'.format(age.days)
+        elif age.seconds >= 3600:
+            repr = '{} hours'.format(age.seconds // 3600)
+        elif age.seconds >= 60:
+            repr = '{} minutes'.format(age.seconds // 60)
+        else:
+            repr = '{} seconds'.format(age.seconds)
+        # Remove the ending 's' if we have only 1 second/minute/hour/day.
+        return repr if not repr.startswith('1 ') else repr[:-1]
+
+    def is_negative(age):
+        return age.total_seconds() < 0
+
+    if is_negative(age):
+        return '-' + format_nonnegative_age(abs(age))
+    return format_nonnegative_age(age)
 
 
 def format_date(date):
